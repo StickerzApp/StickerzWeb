35ff94f52ce8eafe12cc8aa4f6184872504c37fe
tomaisvip <tomaisvip@7686d296-e82f-4e13-84a4-fc58ef4f0099>
2015-09-14 13:27:08 +0000

    Criação da engine de rede

--- src/br/com/stickerz/actions/IndexAction.java
+++ src/br/com/stickerz/actions/IndexAction.java
@@ -1,16 +1,51 @@
 package br.com.stickerz.actions;
 
 
+import java.util.List;
+
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Result;
+
+import br.com.stickerz.model.Rede;
+import br.com.stickerz.service.RedeService;
 
 
 public class IndexAction extends BaseAction {
 	
 	private static final long serialVersionUID = 1L;
+	
+	private RedeService redeService;
+	private List<Rede> listaRedes;
+	
 
 	@Action(value="index",results={@Result(name=SUCCESS, location="acesso.jsp")})
 	public String index() {
+		try {
+			setListaRedes(redeService.listarRedesAtivas());
+		} catch (Exception e) {
+			setListaRedes(null);
+		}
+		
 		return SUCCESS;
+	}
+
+
+	public RedeService getRedeService() {
+		return redeService;
+	}
+
+
+	public void setRedeService(RedeService redeService) {
+		this.redeService = redeService;
+	}
+
+
+	public List<Rede> getListaRedes() {
+		return listaRedes;
+	}
+
+
+	public void setListaRedes(List<Rede> listaRedes) {
+		this.listaRedes = listaRedes;
 	}
 }
